76fd0beeeeccdb21c4d4138f83caf5280d16c3e4
ayoub <Elma0076@algonquinlive.com>
2024-06-06 00:30:10 -0400

    tenth commit: add the configuration source the appsetting Api url

--- GameStore.Frontend/Program.cs
+++ GameStore.Frontend/Program.cs
@@ -6,8 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddSingleton<GamesClient>(); //we added this just to register the services so the service provider knows about them 
-builder.Services.AddSingleton<GenresClient>(); //we added this just to register the services so the service provider knows about them 
+
+    var gameStoreApiUrl = builder.Configuration["GameStoreApiUrl"]??
+        throw new Exception("GameStoreApiUrl is not set");
+
+    builder.Services.AddHttpClient<GamesClient>(
+        client => client.BaseAddress = new Uri(gameStoreApiUrl));  //we registered our httpClient instance / this method to receive the type of the client class that we are going to  use for interacting with the httpclient
+
+    builder.Services.AddHttpClient<GenresClient>(
+        client => client.BaseAddress = new Uri(gameStoreApiUrl));
+
+//now we will delete those two cz at the httpClient is going to register both gameClient and GenresClient with the correspondeing httpClient
+//builder.Services.AddSingleton<GamesClient>(); //we added this just to register the services so the service provider knows about them 
+//builder.Services.AddSingleton<GenresClient>(); //we added this just to register the services so the service provider knows about them 
 
 var app = builder.Build();
 
--- GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
+++ GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a40a398dad1008bf346755bcb04fa1f293fce029")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f0097a633097a89956b228c69eb51ff027f64f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
