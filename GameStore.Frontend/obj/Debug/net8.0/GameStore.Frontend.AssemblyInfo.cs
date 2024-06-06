a40a398dad1008bf346755bcb04fa1f293fce029
ayoub <Elma0076@algonquinlive.com>
2024-06-05 21:27:26 -0400

    eighth commit: add Delete Btn and logic and the popup but it still need the logic to make the data disapear from the form

--- GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
+++ GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b926f158efe6bfdfbd0fed77a03fcc8c66dafd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb4507dcc98a3af998d1d6ca593174d0963c53f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
